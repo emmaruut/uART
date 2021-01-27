d15ba06bd479a6bb86f37f8b8a7919c585d9b45e
Glitch (hello-webpage) <none>
2021-01-27 16:53:54 +0000

    👂👚 Checkpoint ./script.js:23957060/295

--- script.js
+++ script.js
@@ -2,4 +2,17 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+let showMenu = false;
+const mobileMenu = document.getElementById('mobile-menu');
+
+function toggleMenu() {
+  
+  if (showMenu === true){
+    showMenu = false;
+    mobileMenu.classList.add('hidden');
+  }else {
+    showMenu = true;
+    mobileMenu.classList.remove('hidden');
+  }
+}
+
